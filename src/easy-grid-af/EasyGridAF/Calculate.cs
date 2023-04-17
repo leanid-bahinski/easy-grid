8d8b0d56204aa0bb7d81845fb8c578921eac3357
leanid-bahinski <123372505+leanid-bahinski@users.noreply.github.com>
2023-04-17 18:55:42 +0200

    INIT: Create AF project

---
+++ src/easy-grid-af/EasyGridAF/Calculate.cs
@@ -0,0 +1,27 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+
+namespace EasyGridAF
+{
+    public static class Calculate
+    {
+        [FunctionName("Calculate")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            string name = req.Query["name"];
+            int x = int.Parse(req.Query["x"]);
+            int y = int.Parse(req.Query["y"]);
+            int result = x + y;
+
+            return new OkObjectResult(result);
+        }
+    }
+}
